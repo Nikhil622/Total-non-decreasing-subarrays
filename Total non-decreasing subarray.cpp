9591d048d9201427ea3667391217a60423a4c27e
Nikhil622 <71211237+Nikhil622@users.noreply.github.com>
2021-06-01 02:51:32 +0530

    Update Total non-decreasing subarray.cpp

--- Total non-decreasing subarray.cpp
+++ Total non-decreasing subarray.cpp
@@ -13,11 +13,11 @@ int main() {
 	     cin>>a[i];
 	   t[0]=1;
 	   long long x=0;
-	   for(int i=1;i<n;++i)// t[i]: Total non_decreasing subarrays ending with ith element
+	   for(int i=1;i<n;++i)// t[i]: Total non_decreasing subarrays ending with ith element 
 	     {   t[i]=1;
-	         if(a[i-1]<=a[i])
-	           t[i]+=t[i-1];
-	         x+=t[i];
+	         if(a[i-1]<=a[i])  // if ith element is greator or equal to its previous element then total non-decreasing subarrays t[i] will be t[i]+=t[i-1]
+	           t[i]+=t[i-1];   // else total subarrays will be non decreasing subarray will be 1 . ie. subarray cointaining that array itself.
+	         x+=t[i];          // If we dont want we do not require extra t[n] array because only one previous elements value is required.
 	     }
 	   cout<<x+1<<endl;
 	}
